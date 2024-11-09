18021e9625bf1d7b8c9d8f2574520bb563d65fda
0xdeb7ef <45214659+0xdeb7ef@users.noreply.github.com>
2024-11-09 18:26:22 -0500

    finished all opcodes, and poloshed some tests

--- src/chip8.zig
+++ src/chip8.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const random = std.Random.DefaultPrng;
 
 pub const CPU = struct {
     memory: [4 * 1024]u8 = undefined,
@@ -15,8 +16,9 @@ pub const CPU = struct {
     keyboard: [16]bool = [_]bool{false} ** 16,
     display: [HEIGHT][WIDTH]u1 = undefined,
 
-    // config
-    old_shr: bool = false,
+    /// The original COSMAC VIP set the Vx register to Vy before shifting.
+    /// This option allows you to control that behaviour.
+    old_shift: bool = false,
 
     pub const PC_START = 0x200;
     const STACK_SIZE = 16;
@@ -66,8 +68,16 @@ pub const CPU = struct {
         if (self.SP <= 0)
             @panic("stack underflow");
 
-        self.PC = self.stack[self.SP];
         self.SP -= 1;
+        self.PC = self.stack[self.SP];
+    }
+
+    fn get_key(self: *CPU) union(enum) { key: u4, no_key: void } {
+        for (self.keyboard, 0..) |k, i| {
+            if (k) return .{ .key = @truncate(i) };
+        }
+
+        return .no_key;
     }
 
     pub fn fetch(self: *CPU) u16 {
@@ -164,7 +174,7 @@ pub const CPU = struct {
     }
 
     // 00E0 - CLS
-    /// Clear the display.
+    /// Clear the display
     fn CLS(self: *CPU) void {
         for (&self.display) |*d| {
             @memset(d, 0);
@@ -180,53 +190,203 @@ pub const CPU = struct {
     // 1nnn - JP addr
     /// Jump to location nnn
     fn JP(self: *CPU, inst: Instruction) void {
-        self.PC = inst.JP.addr;
+        const addr = inst.JP.addr;
+
+        self.PC = addr;
     }
 
     // 2nnn - CALL addr
     /// Call subroutine at nnn
     fn CALL(self: *CPU, inst: Instruction) void {
+        const addr = inst.CALL.addr;
+
         self.stack_push();
-        self.PC = inst.CALL.addr;
+        self.PC = addr;
     }
 
     // 3xkk - SE Vx, byte
     /// Skip next instruction if Vx = kk
     fn SE_Vx(self: *CPU, inst: Instruction) void {
-        if (self.register[inst.SE_Vx.Vx] == inst.SE_Vx.byte)
+        const Vx = inst.SE_Vx.Vx;
+        const byte = inst.SE_Vx.byte;
+
+        if (self.register[Vx] == byte)
             self.PC += 2;
     }
 
     // 4xkk - SNE Vx, byte
     /// Skip next instruction if Vx != kk
     fn SNE_Vx(self: *CPU, inst: Instruction) void {
-        if (self.register[inst.SNE_Vx.Vx] != inst.SNE_Vx.byte)
+        const Vx = inst.SNE_Vx.Vx;
+        const byte = inst.SNE_Vx.byte;
+
+        if (self.register[Vx] != byte)
             self.PC += 2;
     }
 
     // 5xy0 - SE Vx, Vy
     /// Skip next instruction if Vx = Vy
     fn SE_Vx_Vy(self: *CPU, inst: Instruction) void {
-        if (self.register[inst.SE_Vx_Vy.Vx] == self.register[inst.SE_Vx_Vy.Vy])
+        const Vx = inst.SE_Vx_Vy.Vx;
+        const Vy = inst.SE_Vx_Vy.Vy;
+
+        if (self.register[Vx] == self.register[Vy])
             self.PC += 2;
     }
 
     // 6xkk - LD Vx, byte
     /// Set Vx = kk
     fn LD_Vx(self: *CPU, inst: Instruction) void {
-        self.register[inst.LD_Vx.Vx] = inst.LD_Vx.byte;
+        const Vx = inst.LD_Vx.Vx;
+        const byte = inst.LD_Vx.byte;
+
+        self.register[Vx] = byte;
     }
 
     // 7xkk - ADD Vx, byte
     /// Set Vx = Vx + kk
     fn ADD_Vx(self: *CPU, inst: Instruction) void {
-        self.register[inst.ADD_Vx.Vx] +%= inst.ADD_Vx.byte;
+        const Vx = inst.ADD_Vx.Vx;
+        const byte = inst.ADD_Vx.byte;
+
+        self.register[Vx] +%= byte;
+    }
+
+    // 8xy0 - LD Vx, Vy
+    /// Set Vx = Vy
+    fn LD_Vx_Vy(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_Vx_Vy.Vx;
+        const Vy = inst.LD_Vx_Vy.Vy;
+
+        self.register[Vx] = self.register[Vy];
+    }
+
+    // 8xy1 - OR Vx, Vy
+    /// Set Vx = Vx OR Vy
+    fn OR(self: *CPU, inst: Instruction) void {
+        const Vx = inst.OR.Vx;
+        const Vy = inst.OR.Vy;
+
+        self.register[Vx] |= self.register[Vy];
+    }
+
+    // 8xy2 - AND Vx, Vy
+    /// Set Vx = Vx AND Vy
+    fn AND(self: *CPU, inst: Instruction) void {
+        const Vx = inst.AND.Vx;
+        const Vy = inst.AND.Vy;
+
+        self.register[Vx] &= self.register[Vy];
+    }
+
+    // 8xy3 - XOR Vx, Vy
+    /// Set Vx = Vx XOR Vy
+    fn XOR(self: *CPU, inst: Instruction) void {
+        const Vx = inst.XOR.Vx;
+        const Vy = inst.XOR.Vy;
+
+        self.register[Vx] ^= self.register[Vy];
+    }
+
+    // 8xy4 - ADD Vx, Vy
+    /// Set Vx = Vx + Vy, set VF = carry
+    fn ADD_Vx_Vy(self: *CPU, inst: Instruction) void {
+        const Vx = inst.ADD_Vx_Vy.Vx;
+        const Vy = inst.ADD_Vx_Vy.Vy;
+
+        const a = @addWithOverflow(self.register[Vx], self.register[Vy]);
+
+        // Vx has to be set BEFORE VF!
+        self.register[Vx] = a.@"0";
+        self.register[0xF] = a.@"1";
+    }
+
+    // 8xy5 - SUB Vx, Vy
+    /// Set Vx = Vx - Vy, set VF = NOT borrow
+    fn SUB(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SUB.Vx;
+        const Vy = inst.SUB.Vy;
+
+        const o: u8 = if (self.register[Vx] >= self.register[Vy]) 1 else 0;
+
+        self.register[Vx] -%= self.register[Vy];
+        self.register[0xF] = o;
+    }
+
+    // 8xy6 - SHR Vx {, Vy}
+    /// Set Vx = Vx SHR 1
+    fn SHR(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SHR.Vx;
+        const Vy = inst.SHR.Vy;
+
+        if (self.old_shift)
+            self.register[Vx] = self.register[Vy];
+
+        const f = self.register[Vx] & 1;
+        self.register[Vx] >>= 1;
+        self.register[0xF] = f;
+    }
+
+    // 8xy7 - SUBN Vx, Vy
+    /// Set Vx = Vy - Vx, set VF = NOT borrow
+    fn SUBN(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SUBN.Vx;
+        const Vy = inst.SUBN.Vy;
+
+        const o: u8 = if (self.register[Vy] >= self.register[Vx]) 1 else 0;
+        self.register[Vx] = self.register[Vy] -% self.register[Vx];
+        self.register[0xF] = o;
+    }
+
+    // 8xyE - SHL Vx {, Vy}
+    /// Set Vx = Vx SHL 1
+    fn SHL(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SHL.Vx;
+        const Vy = inst.SHL.Vy;
+
+        if (self.old_shift)
+            self.register[Vx] = self.register[Vy];
+
+        const f = (self.register[Vx] & (1 << 7)) >> 7;
+        self.register[Vx] <<= 1;
+        self.register[0xF] = f;
+    }
+
+    // 9xy0 - SNE Vx, Vy
+    /// Skip next instruction if Vx != Vy
+    fn SNE_Vx_Vy(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SNE_Vx_Vy.Vx;
+        const Vy = inst.SNE_Vx_Vy.Vy;
+
+        if (self.register[Vx] != self.register[Vy])
+            self.PC += 2;
     }
 
     // Annn - LD I, addr
     /// Set I = nnn
     fn LD_I(self: *CPU, inst: Instruction) void {
-        self.I = inst.LD_I.addr;
+        const addr = inst.LD_I.addr;
+
+        self.I = addr;
+    }
+
+    // Bnnn - JP V0, addr
+    /// Jump to location nnn + V0
+    fn JP_V0(self: *CPU, inst: Instruction) void {
+        const addr = inst.JP_V0.addr;
+
+        self.PC = addr + self.register[0x0];
+    }
+
+    // Cxkk - RND Vx, byte
+    /// Set Vx = random byte AND kk
+    fn RND(self: *CPU, inst: Instruction) void {
+        const Vx = inst.RND.Vx;
+        const byte = inst.RND.byte;
+
+        var rand = random.init(0);
+        const r = std.Random.int(rand.random(), u8);
+        self.register[Vx] = r & byte;
     }
 
     // Dxyn - DRW Vx, Vy, nibble
@@ -251,6 +411,112 @@ pub const CPU = struct {
                     self.display[row + r][col + c] = 1;
                 }
             }
+        }
+    }
+
+    // Ex9E - SKP Vx
+    /// Skip next instruction if key with the value of Vx is pressed
+    fn SKP(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SKP.Vx;
+
+        if (self.keyboard[Vx]) {
+            self.PC += 2;
+        }
+    }
+
+    // ExA1 - SKNP Vx
+    /// Skip next instruction if key with the value of Vx is not pressed
+    fn SKNP(self: *CPU, inst: Instruction) void {
+        const Vx = inst.SKNP.Vx;
+
+        if (!self.keyboard[Vx]) {
+            self.PC += 2;
+        }
+    }
+
+    // Fx07 - LD Vx, DT
+    /// Set Vx = delay timer value
+    fn LD_DT(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_DT.Vx;
+
+        self.register[Vx] = self.delay;
+    }
+
+    // Fx0A - LD Vx, K
+    /// Wait for a key press, store the value of the key in Vx
+    fn LD_K(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_K.Vx;
+
+        const key = self.get_key();
+        switch (key) {
+            .key => |k| {
+                self.register[Vx] = k;
+            },
+            .no_key => {
+                self.PC -= 2;
+            },
+        }
+    }
+
+    // Fx15 - LD DT, Vx
+    /// Set delay timer = Vx
+    fn LD_tDT(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_tDT.Vx;
+
+        self.delay = self.register[Vx];
+    }
+
+    // Fx18 - LD ST, Vx
+    // Set sound timer = Vx
+    fn LD_tST(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_tST.Vx;
+
+        self.sound = self.register[Vx];
+    }
+
+    // Fx1E - ADD I, Vx
+    /// Set I = I + Vx
+    fn ADD_I(self: *CPU, inst: Instruction) void {
+        const Vx = inst.ADD_I.Vx;
+
+        self.I += self.register[Vx];
+    }
+
+    // Fx29 - LD F, Vx
+    /// Set I = location of sprite for digit Vx
+    fn LD_F(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_F.Vx;
+
+        self.I = CPU.FONT_START + Vx;
+    }
+
+    // Fx33 - LD B, Vx
+    /// Store BCD representation of Vx in memory locations I, I+1, and I+2
+    fn LD_B(self: *CPU, inst: Instruction) void {
+        const v = self.register[inst.LD_B.Vx];
+
+        self.memory[self.I] = v / 100;
+        self.memory[self.I + 1] = (v / 10) % 10;
+        self.memory[self.I + 2] = v % 10;
+    }
+
+    // Fx55 - LD [I], Vx
+    /// Store registers V0 through Vx in memory starting at location I
+    fn LD_wI(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_wI.Vx;
+
+        for (0..Vx + 1) |i| {
+            self.memory[self.I + i] = self.register[i];
+        }
+    }
+
+    // Fx65 - LD Vx, [I]
+    /// Read registers V0 through Vx from memory starting at location I
+    fn LD_rI(self: *CPU, inst: Instruction) void {
+        const Vx = inst.LD_rI.Vx;
+
+        for (0..Vx + 1) |i| {
+            self.register[i] = self.memory[self.I + i];
         }
     }
 };
@@ -301,6 +567,7 @@ const Instruction = union(enum) {
 };
 
 test "cpu fetch" {
+    if (false) return error.SkipZigTest;
     var c = CPU{ .PC = 0 };
     c.memory[0] = 0xBE;
     c.memory[1] = 0xEF;
@@ -308,36 +575,10 @@ test "cpu fetch" {
     try std.testing.expectEqual(0xBEEF, c.fetch());
 }
 
-test "ibm.ch8" {
-    const rom = @embedFile("ibm.ch8");
-    var c = CPU.init();
-
-    c.load_mem(CPU.PC_START, rom);
-
-    var prev: u16 = undefined;
-    var run = true;
-    while (run) {
-        const i = c.fetch();
-        const ii = CPU.decode(i);
-
-        std.debug.print("{}\n", .{ii});
-
-        try c.execute(ii);
-
-        if (prev == i)
-            run = false;
-        prev = i;
-    }
-
-    for (c.display) |col| {
-        for (col) |d| {
-            std.debug.print("{s}", .{if (d == 1) "\u{2588}" else " "});
-        }
-        std.debug.print("\n", .{});
-    }
-}
-
 test "1-chip8-logo.ch8" {
+    if (false) return error.SkipZigTest;
+    std.debug.print("1-chip-logo.ch8\n", .{});
+
     const rom = @embedFile("1-chip8-logo.ch8");
     var c = CPU.init();
 
@@ -349,7 +590,7 @@ test "1-chip8-logo.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -367,6 +608,9 @@ test "1-chip8-logo.ch8" {
 }
 
 test "2-ibm-logo.ch8" {
+    if (false) return error.SkipZigTest;
+    std.debug.print("2-ibm-logo.ch8:\n", .{});
+
     const rom = @embedFile("2-ibm-logo.ch8");
     var c = CPU.init();
 
@@ -378,7 +622,7 @@ test "2-ibm-logo.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -396,6 +640,9 @@ test "2-ibm-logo.ch8" {
 }
 
 test "3-corax+.ch8" {
+    if (false) return error.SkipZigTest;
+    std.debug.print("3-corax+.ch8:\n", .{});
+
     const rom = @embedFile("3-corax+.ch8");
     var c = CPU.init();
 
@@ -407,7 +654,7 @@ test "3-corax+.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -422,27 +669,41 @@ test "3-corax+.ch8" {
         }
         std.debug.print("\n", .{});
     }
+
+    // yes, we are checking the pixels to see if we passed the tests
+    var i: usize = 3;
+    while (i <= 11) : (i += 5) {
+        var j: usize = 11;
+        while (j < 64) : (j += 16) {
+            try std.testing.expectEqual(1, c.display[i][j]);
+        }
+    }
+
+    i = 23;
+    while (i <= 11) : (i += 5) {
+        var j: usize = 11;
+        while (j <= 43) : (j += 16) {
+            try std.testing.expectEqual(1, c.display[i][j]);
+        }
+    }
 }
 
 test "4-flags.ch8" {
+    if (false) return error.SkipZigTest;
+    std.debug.print("4-flags.ch8:\n", .{});
+
     const rom = @embedFile("4-flags.ch8");
     var c = CPU.init();
 
     c.load_mem(CPU.PC_START, rom);
 
-    var prev: u16 = undefined;
-    var run = true;
-    while (run) {
+    for (0..1000) |_| {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
-
-        if (prev == i)
-            run = false;
-        prev = i;
     }
 
     for (c.display) |col| {
@@ -451,11 +712,49 @@ test "4-flags.ch8" {
         }
         std.debug.print("\n", .{});
     }
+
+    // yes, we are, once again, checking the display pixels
+    try std.testing.expect(check_pixels_flags(c, 2, 27, 3));
+    try std.testing.expect(check_pixels_flags(c, 2, 49, 3));
+
+    try std.testing.expect(check_pixels_flags(c, 7, 5, 3));
+    try std.testing.expect(check_pixels_flags(c, 7, 27, 4));
+    try std.testing.expect(check_pixels_flags(c, 7, 49, 4));
+
+    try std.testing.expect(check_pixels_flags(c, 12, 5, 3));
+    try std.testing.expect(check_pixels_flags(c, 12, 27, 4));
+    try std.testing.expect(check_pixels_flags(c, 12, 49, 3));
+
+    try std.testing.expect(check_pixels_flags(c, 18, 27, 4));
+    try std.testing.expect(check_pixels_flags(c, 18, 49, 4));
+
+    try std.testing.expect(check_pixels_flags(c, 23, 5, 3));
+    try std.testing.expect(check_pixels_flags(c, 23, 27, 4));
+    try std.testing.expect(check_pixels_flags(c, 23, 49, 3));
+
+    try std.testing.expect(check_pixels_flags(c, 29, 31, 2));
+}
+
+fn check_pixels_flags(cpu: CPU, row: usize, col: usize, n: usize) bool {
+    var i: usize = 0;
+    while (i < n) : (i += 1) {
+        if (cpu.display[row][col + (i * 4)] == 0)
+            return false;
+    }
+
+    return true;
 }
 
 test "5-quirks.ch8" {
+    // TODO - Add timer and other stuff to make this test work
+    if (true) return error.SkipZigTest;
+    std.debug.print("5-quirks.ch8:\n", .{});
+
     const rom = @embedFile("5-quirks.ch8");
     var c = CPU.init();
+
+    // auto-select CHIP-8 mode
+    c.memory[0x1FF] = 1;
 
     c.load_mem(CPU.PC_START, rom);
 
@@ -465,7 +764,7 @@ test "5-quirks.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -483,6 +782,10 @@ test "5-quirks.ch8" {
 }
 
 test "6-keypad.ch8" {
+    // TODO - Add keyboard support
+    if (true) return error.SkipZigTest;
+    std.debug.print("6-keypad.ch8:\n", .{});
+
     const rom = @embedFile("6-keypad.ch8");
     var c = CPU.init();
 
@@ -494,7 +797,7 @@ test "6-keypad.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -512,6 +815,10 @@ test "6-keypad.ch8" {
 }
 
 test "7-beep.ch8" {
+    // TODO - Add beep
+    if (true) return error.SkipZigTest;
+    std.debug.print("7-beep.ch8:\n", .{});
+
     const rom = @embedFile("7-beep.ch8");
     var c = CPU.init();
 
@@ -523,7 +830,7 @@ test "7-beep.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
@@ -541,6 +848,10 @@ test "7-beep.ch8" {
 }
 
 test "8-scrolling.ch8" {
+    // not supported
+    if (true) return error.SkipZigTest;
+    std.debug.print("8-scrolling.ch8:\n", .{});
+
     const rom = @embedFile("8-scrolling.ch8");
     var c = CPU.init();
 
@@ -552,7 +863,7 @@ test "8-scrolling.ch8" {
         const i = c.fetch();
         const ii = CPU.decode(i);
 
-        std.debug.print("{}\n", .{ii});
+        // std.debug.print("{}\n", .{ii});
 
         try c.execute(ii);
 
